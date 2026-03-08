94c565a2b83a739bcf9f1cc59617d9e57b4e1be8
Shreya Kumari <shreyakumari8523@gmail.com>
2026-03-08 14:18:43 +0000

    Changes in devcontainer

--- startup_financial_engine/models/revenue.py
+++ startup_financial_engine/models/revenue.py
@@ -7,3 +7,4 @@ class RevenueModel:
         for units in self.assumptions.monthly_unit_sales: #looping through 12 values
             revenue.append(units * self.assumptions.price_per_unit) #revenue=Units Sold×Price Per Unit
         return revenue #list of revenue generated per month
+    
